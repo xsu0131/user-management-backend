ec501368113d38c02d16e492ba1566f5c561ef11
XiaoSu0131 <xsu0131@gmail.com>
2026-02-03 21:51:03 -0800

    changes to S3 service

--- src/main/java/com/syn/usermanagement/service/S3Service.java
+++ src/main/java/com/syn/usermanagement/service/S3Service.java
@@ -1,8 +1,10 @@
 package com.syn.usermanagement.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
@@ -12,6 +14,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 @Service
+@ConditionalOnBean(S3Client.class)
 public class S3Service {
 
     private final S3Client s3Client;
@@ -26,18 +29,14 @@ public class S3Service {
         this.s3Client = s3Client;
     }
 
-    /**
-     * Upload file to S3 and return the public URL
-     */
     public String uploadFile(MultipartFile file, Long userId) throws IOException {
-        // Generate unique filename
         String originalFilename = file.getOriginalFilename();
-        String extension = originalFilename != null
+        String extension = originalFilename != null && originalFilename.lastIndexOf(".") >= 0
                 ? originalFilename.substring(originalFilename.lastIndexOf("."))
                 : ".jpg";
+
         String fileName = "photos/user_" + userId + "_" + UUID.randomUUID() + extension;
 
-        // Upload to S3
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(fileName)
@@ -46,19 +45,12 @@ public class S3Service {
 
         s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
 
-        // Return the S3 URL
         return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
     }
 
-    /**
-     * Delete file from S3
-     */
     public void deleteFile(String fileUrl) {
-        if (fileUrl == null || fileUrl.isEmpty()) {
-            return;
-        }
+        if (fileUrl == null || fileUrl.isEmpty()) return;
 
-        // Extract key from URL
         String key = fileUrl.substring(fileUrl.lastIndexOf(".com/") + 5);
 
         DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
