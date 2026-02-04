b8a6530ca839a66cee2a5ba52a2706e33f72ff58
XiaoSu0131 <xsu0131@gmail.com>
2026-02-03 21:45:31 -0800

    Changes to S3

--- src/main/java/com/syn/usermanagement/config/S3Config.java
+++ src/main/java/com/syn/usermanagement/config/S3Config.java
@@ -3,30 +3,24 @@ package com.syn.usermanagement.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@ConditionalOnProperty(name = "aws.s3.region")
 public class S3Config {
-
-    @Value("${aws.access-key}")
-    private String accessKey;
-
-    @Value("${aws.secret-key}")
-    private String secretKey;
 
     @Value("${aws.s3.region}")
     private String region;
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
