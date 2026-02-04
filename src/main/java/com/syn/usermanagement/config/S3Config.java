47457673a3674b7e6cff3a2dc92788d5980bfd4d
XiaoSu0131 <xsu0131@gmail.com>
2026-02-03 23:27:11 -0800

    Photoupload working implementation

--- src/main/java/com/syn/usermanagement/config/S3Config.java
+++ src/main/java/com/syn/usermanagement/config/S3Config.java
@@ -5,22 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-@ConditionalOnProperty(name = "aws.s3.region")
+@ConditionalOnProperty(name = "aws.access-key")
 public class S3Config {
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
 
     @Value("${aws.s3.region}")
     private String region;
 
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
